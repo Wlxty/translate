0de3eb507e22b2f968d6d1e9f17ea8e8d8d086c7
Wlxty <kmosc97@gmail.com>
2021-11-24 20:03:03 +0100

    fix

--- cmd/translateapp/main.go
+++ cmd/translateapp/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"translateapp/internal"
 	"log"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
+	var dbstatus = internal.SetConnection()
+	fmt.Println(dbstatus)
 	log.Printf("starting...")
 	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer done()
