14670852b509ba3147b5482897e51c4d90262aa6
Wlxty <94838742+Wlxty@users.noreply.github.com>
2021-11-24 19:36:50 +0100

    Task [TR-002] Set up Postgres Database (#2)

---
+++ internal/connection.go
@@ -0,0 +1,30 @@
+package internal
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+var Conn *sql.DB
+
+func ConnectionInfo() string {
+	host, port, user, password, database := Credentials()
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s " + "password=%s dbname=%s sslmode=disable", host, port, user, password, database)
+  return psqlInfo
+}
+
+func SetConnection() error{
+	db, err := sql.Open("postgres", ConnectionInfo())
+	if err != nil {
+		return errors.New("401")
+	}
+	Conn = db
+	return errors.New("200")
+}
+
+func CloseConnection(){
+	defer Conn.Close()
+}
+
---
+++ internal/credentials.go
@@ -0,0 +1,15 @@
+package internal
+
+import (
+	"os"
+)
+
+
+func Credentials() (string, int, string, string, string) {
+	host := "localhost"
+	port := 5432
+	user, _ := os.LookupEnv("DATABASE_USER")
+	password, _ := os.LookupEnv("DATABASE_USER_PASSWORD")
+	database, _ := os.LookupEnv("DATABASE_DATABASE")
+	return host, port, user, password, database
+}
