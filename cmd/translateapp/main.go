c9bd074d3d4ffb1a55a0b891649962d949498b15
Wlxty <kmosc97@gmail.com>
2021-11-26 09:45:59 +0100

    Set up Connection

--- cmd/translateapp/main.go
+++ cmd/translateapp/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	"context"
+	//"context"
 	"fmt"
 	"translateapp/internal"
 	"log"
-	"os/signal"
-	"syscall"
+	//"os/signal"
+	//"syscall"
+	"net/http"
+	servers "translateapp/internal/server"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	var dbstatus = internal.SetConnection()
 	fmt.Println(dbstatus)
 	log.Printf("starting...")
-	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	server := servers.NewTaskServer()
+
+	//create a new router
+	router := mux.NewRouter()
+
+	//specify endpoints, handler functions and HTTP method
+	router.HandleFunc("/languages/", server.TaskHandler).Methods("GET")
+	http.Handle("/", router)
+
+	//start and listen to requests
+	http.ListenAndServe(":8080", router)
+
+	/* ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer done()
 	<-ctx.Done()
-	log.Printf("successful shutdown")
+	log.Printf("successful shutdown") */
 }
